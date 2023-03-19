f0cf72a75621ef0c678e0883e89fd8aae6ebd9a9
AleksMash <amashukov@yandex.ru>
2023-03-19 19:42:01 +0600

    Allow to point the path to the *.json file

--- render_website.py
+++ render_website.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import math
@@ -5,16 +6,19 @@ from pathlib import Path
 from urllib.request import pathname2url
 
 from more_itertools import chunked
+from pathvalidate import sanitize_filepath
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 
 
-def on_reload():
+def on_reload(json_file_path):
+    if not json_file_path:
+        json_file_path = 'book_info.json'
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
-    with open("book_info.json", "r") as file:
+    with open(json_file_path, "r") as file:
         books_json = file.read()
     books = json.loads(books_json)
     for book in books:
@@ -36,8 +40,16 @@ def on_reload():
 
 
 def main():
+    parser = argparse.ArgumentParser(
+        description="Render on-line library site"
+    )
+    parser.add_argument("-jf", "--json_file", type=str,
+                        help="Путь к *.json файлу с данными книг")
+    json_file_path = parser.parse_args().json_file
+    if json_file_path:
+        json_file_path = sanitize_filepath(json_file_path)
     os.makedirs('pages', exist_ok=True)
-    on_reload()
+    on_reload(json_file_path)
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.', default_filename='pages/index.html')

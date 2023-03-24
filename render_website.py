f62c9c7379bca07998da55e17f1fdae69e5f0736
AleksMash <amashukov@yandex.ru>
2023-03-25 00:35:33 +0600

    Fix var names

--- render_website.py
+++ render_website.py
@@ -21,13 +21,13 @@ def on_reload(json_file_path=None):
         autoescape=select_autoescape(['html'])
     )
     with open(json_file_path, "r") as file:
-        books_description = json.load(file)
-    for book_description in books_description:
+        book_descriptions = json.load(file)
+    for book_description in book_descriptions:
         book_description['book_url'] = pathname2url(book_description['book_path'])
-    chunked_books_description = chunked(books_description, BOOK_CARDS_PER_PAGE)
-    page_count = math.ceil(len(books_description)/BOOK_CARDS_PER_PAGE)
+    chunked_book_descriptions = chunked(book_descriptions, BOOK_CARDS_PER_PAGE)
+    page_count = math.ceil(len(book_descriptions)/BOOK_CARDS_PER_PAGE)
     template = env.get_template('template.html')
-    for page_num, book_cards in enumerate(chunked_books_description, start=1):
+    for page_num, book_cards in enumerate(chunked_book_descriptions, start=1):
         rendered_page = template.render(
             books=book_cards,
             page_count=page_count,
@@ -43,8 +43,7 @@ def main():
     )
     parser.add_argument('-jf', '--json_file', type=str,
                         help='Путь к *.json файлу с данными книг', default='book_info.json')
-    json_file_path = parser.parse_args().json_file
-    json_file_path = sanitize_filepath(json_file_path)
+    json_file_path = sanitize_filepath(parser.parse_args().json_file)
     os.makedirs('pages', exist_ok=True)
     on_reload(json_file_path)
     server = Server()
